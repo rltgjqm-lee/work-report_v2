79057f48b37bcf1b4b16179e7f828c3e2d789cad
ChaeEun <ichenny.lee@gmail.com>
2026-07-03 21:27:11 +0900

    chore : prettier 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettierPlugin from "eslint-plugin-prettier"; // 1. 플러그인 가져오기
+import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      // 옵션은 .prettierrc.json 에서 읽음 (단일 소스)
+      "prettier/prettier": "error",
+    },
   },
-])
+]);
