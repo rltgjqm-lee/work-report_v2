41683ae03f9770062002a29bd3e9d4d26af6f660
ChaeEun <ichenny.lee@gmail.com>
2026-07-04 10:35:46 +0900

    style : 1 페이지 스타일 수정

--- src/Pages/main_pages/Page1OnConfig.tsx
+++ src/Pages/main_pages/Page1OnConfig.tsx
@@ -70,7 +70,6 @@ const Page1OnConfig = ({
     <div
       className="p-[30px_20px] flex-1 flex flex-col max-[600px]:p-[20px_15px]"
       id="page1"
-      style={{ display: "flex" }}
     >
       <div className="text-[22px] font-bold mb-[25px] text-[#2c3e50] text-left whitespace-nowrap tracking-[-0.5px] max-[600px]:text-[20px] max-[600px]:mb-[18px]">
         노인공익활동사업 활동일지
@@ -126,7 +125,7 @@ const Page1OnConfig = ({
       </div>
 
       {/* 액션 버튼 */}
-      <div className="flex justify-center gap-2 mt-auto pt-5 max-[600px]:mt-5 max-[600px]:pt-0">
+      <div className="flex justify-center gap-4 mt-auto pt-5 max-[600px]:mt-5 max-[600px]:pt-0">
         <Button variant="blue" onClick={handleClickSaveButton}>
           저장하기
         </Button>
