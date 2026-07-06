1ee933a222f5e21d3d59e570652cb55fc4c239ad
ChaeEun <ichenny.lee@gmail.com>
2026-07-06 16:05:09 +0900

    feat : admin 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import Main from "./pages/Main";
+import Admin from "./pages/Admin";
 
 import { startGeofenceTracking, stopGeofenceTracking } from "./utils/geofence";
 
@@ -44,7 +45,8 @@ function App() {
       <BrowserRouter>
         {/* <Header /> */}
         <Routes>
-          <Route path="/" element={<Main />}></Route>
+          <Route path="/*" element={<Main />}></Route>
+          <Route path="/admin" element={<Admin />}></Route>
           {/* <Route path="/product/*" element={<Product />}></Route> */}
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
           {/* <Route path="*" element={<NotFound />}></Route> */}
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,4 @@
+const Admin = () => {
+  return <div>Admin 페이지입니다. </div>;
+};
+export default Admin;
